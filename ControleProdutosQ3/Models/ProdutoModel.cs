19b14ca31f832731d50c51a5b163167b9f515b94
Guilherme Cruz <deepdholphin69@gmail.com>
2023-09-23 13:15:46 -0300

    Adicionar arquivos de projeto.

---
+++ ControleProdutosQ3/Models/ProdutoModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ControleProdutosQ3.Models
+{
+    public class ProdutoModel
+    {
+        public Int64 id { get; set; }
+        [Required] public string CodigoDeBarras { get; set; }
+        [Required] public string Descricao { get; set;}
+        public DateTime DataDeValidade { get; set; }
+        public DateTime DataDeRegistro { get; set; }
+        public int Quantidade {  get; set; }
+        public decimal? Valor { get; set; }
+        [MaybeNull] public string NomeDaFoto { get; set; }
+        [MaybeNull] public byte[] Foto {  get; set; }
+        public bool Ativo {  get; set; }
+        
+    }
+}
